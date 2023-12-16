f30d5cd0600a0ddc1055c076c205c2937220289b
K-MHOON <azmria117@gmail.com>
2023-12-16 19:30:56 +0900

    feat[web] : SpringBootApplication 스캔 패키지 설정

--- web/src/main/java/com/kmhoon/web/WebApplication.java
+++ web/src/main/java/com/kmhoon/web/WebApplication.java
@@ -3,7 +3,7 @@ package com.kmhoon.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.kmhoon")
 public class WebApplication {
 
     public static void main(String[] args) {
