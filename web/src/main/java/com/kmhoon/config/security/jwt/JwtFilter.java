5a2b09544f12351d0648c2cc6ea4d7921e5434b2
K-MHOON <azmria117@gmail.com>
2023-12-18 19:17:04 +0900

    feat[web] : Jwt 인증 필터 추가

---
+++ web/src/main/java/com/kmhoon/config/security/jwt/JwtFilter.java
@@ -0,0 +1,58 @@
+package com.kmhoon.config.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final HandlerExceptionResolver resolver;
+
+    public JwtFilter(JwtTokenProvider jwtTokenProvider, @Qualifier("handlerExceptionResolver") HandlerExceptionResolver handlerExceptionResolver) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.resolver = handlerExceptionResolver;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = getToken(request, response);
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception ex) {
+            this.resolver.resolveException(request, response, null, ex);
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request, HttpServletResponse response) {
+        String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if(!StringUtils.hasText(bearerToken) || !bearerToken.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("Bearer 헤더가 존재하지 않습니다.");
+        }
+
+        String token = bearerToken.substring(7);
+
+        if(!jwtTokenProvider.validate(token)) {
+            throw new IllegalArgumentException("토큰 검증에 실패했습니다.");
+        }
+
+        return token;
+    }
+
+}
