bc6729ecffcd3d7ac68edac77cf52f02cb8210c7
K-MHOON <azmria117@gmail.com>
2023-12-19 21:55:48 +0900

    feat[web] : Jwt TokenProvider 추가

---
+++ web/src/main/java/com/kmhoon/config/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,98 @@
+package com.kmhoon.config.security.jwt;
+
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final JwtKey jwtKey;
+    private final JwtTime jwtTime;
+
+    public TokenInfo generate(Authentication authentication) {
+
+        String authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+
+        Date accessTokenExpiresIn = new Date(now + jwtTime.getAccessTokenTime());
+
+        String accessToken = Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim("auth", authorities)
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(jwtKey.getKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+        Date refreshExpiresIn = new Date(now + jwtTime.getRefreshTokenTime());
+
+        String refreshToken = Jwts.builder()
+                .setSubject(authentication.getName())
+                .setExpiration(refreshExpiresIn)
+                .signWith(jwtKey.getKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+        return TokenInfo.builder()
+                .grantType("Bearer")
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        Claims claims = parseClaims(accessToken);
+        if(claims.get("auth") == null) {
+            throw new IllegalArgumentException("Jwt Auth Exception");
+        }
+
+        List<SimpleGrantedAuthority> authorities = Arrays.stream(claims.get("auth").toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        User user = new User(claims.getSubject(), "", authorities);
+        return new JwtAuthentication(user, "", authorities);
+    }
+
+    public boolean validate(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(jwtKey.getKey()).build().parseClaimsJws(token);
+            return true;
+        }catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.", e);
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.", e);
+        }
+        return false;
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(jwtKey.getKey())
+                    .build()
+                    .parseClaimsJws(accessToken)
+                    .getBody();
+        } catch (ExpiredJwtException ex) {
+            return ex.getClaims();
+        }
+    }
+}
