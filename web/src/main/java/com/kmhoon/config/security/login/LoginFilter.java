de1695ce9a398d7675ca5a6ddc486e47ec1c44cd
K-MHOON <azmria117@gmail.com>
2023-12-20 21:33:33 +0900

    feat[web] : 로그인 필터 추가

---
+++ web/src/main/java/com/kmhoon/config/security/login/LoginFilter.java
@@ -0,0 +1,20 @@
+package com.kmhoon.config.security.login;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        return super.requiresAuthentication(request, response);
+    }
+}
