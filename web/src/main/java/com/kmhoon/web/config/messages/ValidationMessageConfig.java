374f2568b7151c68afa74a5b6043a13583abfe81
K-MHOON <azmria117@gmail.com>
2023-12-25 17:37:24 +0900

    feat : ValidationMessageConfig 추가

---
+++ web/src/main/java/com/kmhoon/web/config/messages/ValidationMessageConfig.java
@@ -0,0 +1,36 @@
+package com.kmhoon.web.config.messages;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Properties;
+
+@Configuration
+public class ValidationMessageConfig implements WebMvcConfigurer {
+
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+    private MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCommonMessages(yamlProperties());
+        return messageSource;
+    }
+
+    private Properties yamlProperties() {
+        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
+        bean.setResources(new ClassPathResource("messages.yml"));
+        return bean.getObject();
+    }
+}
