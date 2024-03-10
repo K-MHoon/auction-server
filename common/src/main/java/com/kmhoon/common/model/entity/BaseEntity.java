c775f703f233a4be4f4403ab021b0035a747d5e8
K-MHOON <azmria117@gmail.com>
2024-03-10 22:33:15 +0900

    feat : 생성자, 수정자가 포함된 BaseEntity 추가

---
+++ common/src/main/java/com/kmhoon/common/model/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.kmhoon.common.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity extends BaseTimeEntity {
+
+    @Column(name = "created_by", updatable = false)
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    @LastModifiedBy
+    private String modifiedBy;
+}
