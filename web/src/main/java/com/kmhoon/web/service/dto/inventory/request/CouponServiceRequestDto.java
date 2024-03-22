8d0a677f35d14a57a69454cfccd76afc755233e6
K-MHOON <azmria117@gmail.com>
2024-03-22 20:35:39 +0900

    feat : 쿠폰 서비스 요청 Dto 추가

---
+++ web/src/main/java/com/kmhoon/web/service/dto/inventory/request/CouponServiceRequestDto.java
@@ -0,0 +1,17 @@
+package com.kmhoon.web.service.dto.inventory.request;
+
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CouponServiceRequestDto {
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @Builder
+    @Getter
+    @ToString
+    public static class Buy {
+
+        private Long count;
+    }
+}
