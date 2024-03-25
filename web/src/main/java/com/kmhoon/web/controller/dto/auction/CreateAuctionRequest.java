44c057bb8cfbd4d3184cad95b29c333eccca65a4
K-MHOON <azmria117@gmail.com>
2024-03-25 21:42:37 +0900

    feat : 경매 생성 요청 컨트롤러 dto 추가

---
+++ web/src/main/java/com/kmhoon/web/controller/dto/auction/CreateAuctionRequest.java
@@ -0,0 +1,43 @@
+package com.kmhoon.web.controller.dto.auction;
+
+import com.kmhoon.web.service.dto.auction.request.AuctionServiceRequestDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Builder
+@ToString
+public class CreateAuctionRequest {
+
+    @NotBlank
+    private String title;
+
+    @NotNull
+    private Long minPrice;
+
+    @NotNull
+    private Long price;
+
+    @NotBlank
+    private String description;
+
+    @UniqueElements
+    @NotNull
+    private List<Long> itemSeqList;
+
+    public AuctionServiceRequestDto.CreateAuctionServiceRequest toServiceRequest() {
+        return AuctionServiceRequestDto.CreateAuctionServiceRequest.builder()
+                .title(this.title)
+                .minPrice(this.minPrice)
+                .price(this.price)
+                .description(this.description)
+                .itemSeqList(this.itemSeqList)
+                .build();
+    }
+}
