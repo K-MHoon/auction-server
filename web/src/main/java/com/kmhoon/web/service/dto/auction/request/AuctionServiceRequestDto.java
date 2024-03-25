b530e5c5031389115d53f00f019194b6334222d3
K-MHOON <azmria117@gmail.com>
2024-03-25 21:42:05 +0900

    feat : 경매 서비스 생성 요청 Dto, 생성 요청 추가

---
+++ web/src/main/java/com/kmhoon/web/service/dto/auction/request/AuctionServiceRequestDto.java
@@ -0,0 +1,23 @@
+package com.kmhoon.web.service.dto.auction.request;
+
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AuctionServiceRequestDto {
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @Getter
+    @ToString
+    @Builder
+    public static class CreateAuctionServiceRequest {
+
+        private String title;
+        private Long minPrice;
+        private Long price;
+        private String description;
+        private List<Long> itemSeqList;
+    }
+}
