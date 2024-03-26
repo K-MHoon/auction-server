9b4b40d40dd27fcd748c64f80b55e99fba27ad8f
K-MHOON <azmria117@gmail.com>
2024-03-26 19:49:14 +0900

    feat : BaseEntity 사용자 ID 정보 저장하도록 설정

--- web/src/main/java/com/kmhoon/web/config/db/JpaConfig.java
+++ web/src/main/java/com/kmhoon/web/config/db/JpaConfig.java
@@ -1,13 +1,28 @@
 package com.kmhoon.web.config.db;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages = "com.kmhoon.common.repository")
 @EntityScan(basePackages = "com.kmhoon.common.model.entity")
 public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null) {
+            return Optional::empty;
+        }
+        return () -> Optional.of(authentication.getName());
+    }
 }
