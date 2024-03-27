42bbef71d59dccefe640662b991c30cbf4ee262a
K-MHOON <azmria117@gmail.com>
2024-03-27 21:16:41 +0900

    feat : DateTime 유틸 추가

---
+++ web/src/main/java/com/kmhoon/web/utils/DateTimeUtil.java
@@ -0,0 +1,20 @@
+package com.kmhoon.web.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DateTimeUtil {
+
+    private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    public static String dateTimeToString(LocalDateTime localDateTime) {
+        if(localDateTime == null) {
+            return "";
+        }
+        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN));
+    }
+}
