49fe6f5cbf49e71713a34b23ea76e76e8545a888
K-MHOON <azmria117@gmail.com>
2024-04-14 20:39:26 +0900

    feat : 스프링 시큐리티 User 기반 로그인용 Dto 추가

---
+++ web/src/main/java/com/kmhoon/web/security/login/UserDto.java
@@ -0,0 +1,27 @@
+package com.kmhoon.web.security.login;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Getter
+@ToString
+public class UserDto extends User {
+
+    private String email;
+    private String name;
+    private String roleNames;
+
+    public UserDto(String email, String name, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(email, password, authorities);
+        this.email = email;
+        this.name = name;
+        this.roleNames = authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+    }
+}
