225a3b9355fbf660945e432b0e00bcf7527d2c5a
K-MHOON <azmria117@gmail.com>
2024-05-02 21:01:02 +0900

    feat : ItemControllerRequest DTO 추가

---
+++ web/src/main/java/com/kmhoon/web/controller/dto/item/request/ItemControllerRequest.java
@@ -0,0 +1,51 @@
+package com.kmhoon.web.controller.dto.item.request;
+
+import com.kmhoon.common.enums.ItemType;
+import com.kmhoon.web.service.dto.item.ItemServiceRequestDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ItemControllerRequest {
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @ToString
+    public static final class Add {
+
+        @NotBlank
+        private String name;
+
+        @NotNull
+        private ItemType type;
+
+        @NotBlank
+        private String description;
+
+        @Builder.Default
+        @NotNull
+        private List<MultipartFile> images = new ArrayList<>();
+
+        @Builder.Default
+        @NotNull
+        private List<MultipartFile> documents = new ArrayList<>();
+
+        public ItemServiceRequestDto.Add toServiceRequest() {
+            return ItemServiceRequestDto.Add.builder()
+                    .name(this.name)
+                    .type(this.type)
+                    .description(this.description)
+                    .images(this.images)
+                    .documents(this.documents)
+                    .build();
+        }
+    }
+}
