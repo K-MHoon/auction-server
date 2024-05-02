fede3cd6210c5cc27604ca6b1f212354080561d2
K-MHOON <azmria117@gmail.com>
2024-05-02 21:01:13 +0900

    feat : ItemController 추가

---
+++ web/src/main/java/com/kmhoon/web/controller/item/ItemController.java
@@ -0,0 +1,29 @@
+package com.kmhoon.web.controller.item;
+
+import com.kmhoon.web.controller.dto.item.request.ItemControllerRequest;
+import com.kmhoon.web.service.item.ItemService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+    @GetMapping("/api/view/item/{fileName}")
+    public ResponseEntity<Resource> viewFileGet(@PathVariable(name = "fileName") String fileName) {
+        return itemService.view(fileName);
+    }
+
+    @PostMapping("/api/service/item")
+    @ResponseStatus(HttpStatus.OK)
+    public void add(ItemControllerRequest.Add request) {
+        log.info("add : " + request);
+        itemService.add(request.toServiceRequest());
+    }
+}
