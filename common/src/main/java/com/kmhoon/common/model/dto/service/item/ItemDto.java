02497005e392c3fedbf21457c2249783a3728564
K-MHOON <azmria117@gmail.com>
2024-05-13 19:18:32 +0900

    feat : ItemDto 상세 내용 추가

--- common/src/main/java/com/kmhoon/common/model/dto/service/item/ItemDto.java
+++ common/src/main/java/com/kmhoon/common/model/dto/service/item/ItemDto.java
@@ -2,7 +2,12 @@ package com.kmhoon.common.model.dto.service.item;
 
 import com.kmhoon.common.enums.ItemType;
 import com.kmhoon.common.model.entity.service.item.Item;
+import com.kmhoon.common.model.entity.service.item.ItemDocument;
+import com.kmhoon.common.model.entity.service.item.ItemImage;
+import com.kmhoon.common.utils.DateTimeUtil;
 import lombok.*;
+
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -12,22 +17,37 @@ import lombok.*;
 public final class ItemDto {
 
     private Long sequence;
-
     private String name;
-
     private String description;
-
     private ItemType type;
-
     private Boolean isUse;
+    private String createdBy;
+    private String createdAt;
+    private List<ItemImage> itemImageList;
+    private List<ItemDocument> itemDocumentList;
 
-    public static ItemDto of(Item item) {
+    public static ItemDto ofSimple(Item item) {
         return ItemDto.builder()
                 .sequence(item.getSequence())
                 .name(item.getName())
                 .description(item.getDescription())
                 .type(item.getType())
                 .isUse(item.getIsUse())
+                .itemImageList(item.getImageList())
+                .build();
+    }
+
+    public static ItemDto ofDetail(Item item) {
+        return ItemDto.builder()
+                .sequence(item.getSequence())
+                .name(item.getName())
+                .description(item.getDescription())
+                .type(item.getType())
+                .isUse(item.getIsUse())
+                .createdBy(item.getCreatedBy())
+                .createdAt(DateTimeUtil.dateTimeToString(item.getCreatedAt()))
+                .itemImageList(item.getImageList())
+                .itemDocumentList(item.getDocumentList())
                 .build();
     }
 }
