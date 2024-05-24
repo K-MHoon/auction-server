450818faa6078dc721779d1ce20ced2800855752
K-MHOON <azmria117@gmail.com>
2024-05-24 19:15:39 +0900

    feat : 경매 생성 Dto에 빠진 필드 추가

--- web/src/main/java/com/kmhoon/web/controller/dto/auction/CreateAuctionRequest.java
+++ web/src/main/java/com/kmhoon/web/controller/dto/auction/CreateAuctionRequest.java
@@ -1,16 +1,19 @@
 package com.kmhoon.web.controller.dto.auction;
 
+import com.kmhoon.common.enums.AuctionType;
 import com.kmhoon.web.service.dto.auction.request.AuctionServiceRequestDto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.validator.constraints.UniqueElements;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
+@Setter
 @Builder
 @ToString
 public class CreateAuctionRequest {
@@ -21,22 +24,32 @@ public class CreateAuctionRequest {
     @NotNull
     private Long minPrice;
 
-    @NotNull
-    private Long price;
-
     @NotBlank
     private String description;
 
     @NotNull
+    private AuctionType type;
+
+    @NotNull
     private Long itemSeq;
+
+    @NotBlank
+    private String startTime;
+
+    @NotBlank
+    private String endTime;
+
+    @NotNull
+    private MultipartFile image;
 
     public AuctionServiceRequestDto.CreateAuctionServiceRequest toServiceRequest() {
         return AuctionServiceRequestDto.CreateAuctionServiceRequest.builder()
                 .title(this.title)
                 .minPrice(this.minPrice)
-                .price(this.price)
                 .description(this.description)
                 .itemSeq(this.itemSeq)
+                .type(this.type)
+                .image(this.image)
                 .build();
     }
 }
--- web/src/main/java/com/kmhoon/web/service/dto/auction/request/AuctionServiceRequestDto.java
+++ web/src/main/java/com/kmhoon/web/service/dto/auction/request/AuctionServiceRequestDto.java
@@ -1,7 +1,11 @@
 package com.kmhoon.web.service.dto.auction.request;
 
+import com.kmhoon.common.enums.AuctionType;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -16,8 +20,11 @@ public class AuctionServiceRequestDto {
 
         private String title;
         private Long minPrice;
-        private Long price;
+        private AuctionType type;
         private String description;
+        private String startTime;
+        private String endTime;
         private Long itemSeq;
+        private MultipartFile image;
     }
 }
