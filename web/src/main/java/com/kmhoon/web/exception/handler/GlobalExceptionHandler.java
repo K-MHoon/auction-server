570f9f0316d6ca195c23238ebd13e7bd1edae441
K-MHOON <azmria117@gmail.com>
2024-06-03 20:45:05 +0900

    feat : GlobalExceptionHandler 추가

--- web/src/main/java/com/kmhoon/web/exception/handler/GlobalExceptionHandler.java
+++ web/src/main/java/com/kmhoon/web/exception/handler/GlobalExceptionHandler.java
@@ -1,10 +1,20 @@
 package com.kmhoon.web.exception.handler;
 
+import com.kmhoon.web.exception.AuctionApiException;
+import com.kmhoon.web.exception.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(AuctionApiException.class)
+    public ResponseEntity<ErrorResponse> handleAuctionApiException(AuctionApiException ex) {
+        log.debug("cause Auction Api Exception = ", ex);
+        return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(ErrorResponse.of(ex.getErrorCode()));
+    }
 }
