6587fd5fcd3c8fba6661e0e5212c5f5964916fb1
K-MHOON <azmria117@gmail.com>
2024-06-07 22:43:07 +0900

    feat : 경매 생성시 제약 조건 추가

--- web/src/main/java/com/kmhoon/web/exception/code/auction/AuctionErrorCode.java
+++ web/src/main/java/com/kmhoon/web/exception/code/auction/AuctionErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AuctionErrorCode implements ErrorCode {
 
-    NOT_FINISHED_AUCTION_EXIST("S-AUCTION-001",HttpStatus.BAD_REQUEST, "해당 물품은 이미 진행중인 경매가 존재합니다."),
+    NOT_FINISHED_AUCTION_EXIST("S-AUCTION-001",HttpStatus.BAD_REQUEST, "해당 물품은 이미 진행 또는 진행 대기중인 경매가 존재합니다."),
+    START_DATE_NOT_GE_END_DATE("S-AUCTION-002",HttpStatus.BAD_REQUEST, "시작일자는 종료일자보다 크거나 같을 수 없습니다."),
+    BETWEEN_ONE_WEEK("S-AUCTION-003",HttpStatus.BAD_REQUEST, "시작일자는 종료일자는 최대 1주일까지 가능합니다."),
+    HAS_NOT_COUPON("S-AUCTION-004",HttpStatus.BAD_REQUEST, "사용가능한 쿠폰이 존재하지 않습니다."),
 
     ;
 
--- web/src/main/java/com/kmhoon/web/service/auction/AuctionService.java
+++ web/src/main/java/com/kmhoon/web/service/auction/AuctionService.java
@@ -1,10 +1,13 @@
 package com.kmhoon.web.service.auction;
 
 import com.kmhoon.common.enums.AuctionStatus;
+import com.kmhoon.common.enums.CouponStatus;
 import com.kmhoon.common.model.entity.auth.user.User;
 import com.kmhoon.common.model.entity.service.auction.Auction;
+import com.kmhoon.common.model.entity.service.inventory.Coupon;
 import com.kmhoon.common.model.entity.service.item.Item;
 import com.kmhoon.common.repository.service.auction.AuctionRepository;
+import com.kmhoon.common.repository.service.inventory.CouponRepository;
 import com.kmhoon.common.repository.service.item.ItemRepository;
 import com.kmhoon.web.exception.AuctionApiException;
 import com.kmhoon.web.exception.code.auction.AuctionErrorCode;
@@ -17,7 +20,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+
+import static com.kmhoon.web.exception.code.auction.AuctionErrorCode.BETWEEN_ONE_WEEK;
+import static com.kmhoon.web.exception.code.auction.AuctionErrorCode.START_DATE_NOT_GE_END_DATE;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +34,7 @@ public class AuctionService {
     private final AuctionRepository auctionRepository;
     private final ItemRepository itemRepository;
     private final UserCommonService userCommonService;
+    private final CouponRepository couponRepository;
     private final CustomFileUtil fileUtil;
 
     @Transactional
@@ -35,9 +43,24 @@ public class AuctionService {
         Item item = itemRepository.findByInventoryAndSequenceAndIsUseIsTrue(loggedInUser.getInventory(), request.getItemSeq())
                 .orElseThrow(() -> new AuctionApiException(ItemErrorCode.HAS_NOT_SEQ_REQUEST));
 
+        List<Coupon> couponList = couponRepository.findAllByInventoryAndIsUseIsTrueAndStatus(loggedInUser.getInventory(), CouponStatus.UNUSED);
+
+        if(couponList.isEmpty()) {
+            throw new AuctionApiException(AuctionErrorCode.HAS_NOT_COUPON);
+        }
+
         // 이미 진행중인 경매가 있는 경우 신규 경매 생성 불가
         if(item.getAuctionList().stream().anyMatch(auction -> auction.getStatus() != AuctionStatus.FINISHED)) {
             throw new AuctionApiException(AuctionErrorCode.NOT_FINISHED_AUCTION_EXIST);
+        }
+
+        if(request.getStartTime().isEqual(request.getEndTime())
+                || request.getStartTime().isAfter(request.getEndTime())) {
+            throw new AuctionApiException(START_DATE_NOT_GE_END_DATE);
+        }
+
+        if(ChronoUnit.WEEKS.between(request.getStartTime(), request.getEndTime()) > 1) {
+            throw new AuctionApiException(BETWEEN_ONE_WEEK);
         }
 
         List<String> uploadImageFileNames = fileUtil.saveFiles(request.getImages());
