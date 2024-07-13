ff097bf949c1269b5c779ddd52fc85d2a73f7718
K-MHOON <azmria117@gmail.com>
2024-07-13 23:17:16 +0900

    fix : 소켓 접근시 사용자 정보 제공하도록 인터셉터 기능 추가

--- web/src/main/java/com/kmhoon/web/config/socket/SocketChannelInterceptor.java
+++ web/src/main/java/com/kmhoon/web/config/socket/SocketChannelInterceptor.java
@@ -3,7 +3,7 @@ package com.kmhoon.web.config.socket;
 import com.kmhoon.web.exception.AuctionApiException;
 import com.kmhoon.web.exception.code.security.SecurityErrorCode;
 import com.kmhoon.web.security.jwt.JwtTokenProvider;
-import io.jsonwebtoken.Claims;
+import com.kmhoon.web.service.auction.AuctionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
@@ -11,14 +11,25 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class SocketChannelInterceptor implements ChannelInterceptor {
 
+    private static final Pattern PARTICIPANT = Pattern.compile("/sub/auction/(\\d+)/participant");
+
     private final JwtTokenProvider jwtTokenProvider;
+    private final AuctionService auctionService;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
@@ -28,8 +39,44 @@ public class SocketChannelInterceptor implements ChannelInterceptor {
             if(jwtToken == null) {
                 throw new AuctionApiException(SecurityErrorCode.VALIDATION_TOKEN);
             }
-            Claims claims = jwtTokenProvider.validate(jwtToken.replace("Bearer ", ""));
-            log.info("Connect = {}", claims.get("email"));
+            Map<String, Object> sessionAttributes = accessor.getSessionAttributes();
+            sessionAttributes.put("user", jwtTokenProvider.getAuthentication(jwtToken.replace("Bearer ", "")));
+            sessionAttributes.put("destination", new HashSet<String>());
+        } else if(accessor.getCommand() == StompCommand.SUBSCRIBE) {
+            Object user = accessor.getSessionAttributes().get("user");
+            if(user == null) {
+                return message;
+            }
+            String destination = accessor.getDestination();
+            Set<String> destinationSet = (HashSet<String>) accessor.getSessionAttributes().get("destination");
+            destinationSet.add(destination);
+
+            SecurityContextHolder.getContext().setAuthentication((UsernamePasswordAuthenticationToken)user);
+
+            Matcher matcher = PARTICIPANT.matcher(destination);
+            if(matcher.matches()) {
+                Long auctionSeq = Long.valueOf(matcher.group(1));
+                auctionService.participate(auctionSeq);
+            }
+        } else if(accessor.getCommand() == StompCommand.DISCONNECT) {
+            Set<String> destinationSet = (HashSet<String>) accessor.getSessionAttributes().get("destination");
+            if(destinationSet == null || destinationSet.isEmpty()) {
+                return message;
+            }
+            Object user = accessor.getSessionAttributes().get("user");
+            if(user == null) {
+                return message;
+            }
+            SecurityContextHolder.getContext().setAuthentication((UsernamePasswordAuthenticationToken)user);
+            // 모든 연결을 끊어야 한다.
+            for (String destination : destinationSet) {
+                Matcher matcher = PARTICIPANT.matcher(destination);
+                if(matcher.matches()) {
+                    Long auctionSeq = Long.valueOf(matcher.group(1));
+                    auctionService.exit(auctionSeq);
+                }
+            }
+            destinationSet.clear();
         }
 
         return message;
