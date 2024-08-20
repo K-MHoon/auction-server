d775e936c888abb8ab51eb0ef3340e505b9c4a65
K-MHOON <azmria117@gmail.com>
2024-08-20 22:19:09 +0900

    fix : 배치 Tasklet 분리된 것 적용

--- batch/src/main/java/com/kmhoon/batch/job/auction/AuctionStatusJobConfig.java
+++ batch/src/main/java/com/kmhoon/batch/job/auction/AuctionStatusJobConfig.java
@@ -1,8 +1,5 @@
 package com.kmhoon.batch.job.auction;
 
-import com.kmhoon.common.enums.AuctionStatus;
-import com.kmhoon.common.model.entity.service.auction.Auction;
-import com.kmhoon.common.repository.service.auction.AuctionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -10,20 +7,13 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Configuration
 @RequiredArgsConstructor
 public class AuctionStatusJobConfig {
-
-    private final AuctionRepository auctionRepository;
 
     @Bean
     public Job auctionStatusJob(JobRepository jobRepository, Step startAuctionStep, Step endAuctionStep) {
@@ -35,26 +25,17 @@ public class AuctionStatusJobConfig {
     }
 
     @Bean
-    public Step startAuctionStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public Step startAuctionStep(JobRepository jobRepository, PlatformTransactionManager transactionManager, AuctionStartTasklet tasklet) {
         return new StepBuilder("startAuctionStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    String date = chunkContext.getStepContext().getJobParameters().get("date").toString();
-                    List<Auction> startTargetAuctionList = auctionRepository.findAllByStartTimeAndIsUseIsTrueAndStatus(LocalDateTime.parse(date).withSecond(0).withNano(0), AuctionStatus.BEFORE);
-                    startTargetAuctionList.forEach(auction -> auction.updateAuctionStatus(AuctionStatus.RUNNING));
-                    return RepeatStatus.FINISHED;
-                    }, transactionManager)
+                .tasklet(tasklet, transactionManager)
                 .build();
     }
 
     @Bean
-    public Step endAuctionStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public Step endAuctionStep(JobRepository jobRepository, PlatformTransactionManager transactionManager, AuctionEndTasklet tasklet) {
         return new StepBuilder("endAuctionStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    String date = chunkContext.getStepContext().getJobParameters().get("date").toString();
-                    List<Auction> startTargetAuctionList = auctionRepository.findAllByEndTimeAndIsUseIsTrueAndStatus(LocalDateTime.parse(date).withSecond(0).withNano(0), AuctionStatus.RUNNING);
-                    startTargetAuctionList.forEach(auction -> auction.updateAuctionStatus(AuctionStatus.FINISHED));
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
+                .tasklet(tasklet, transactionManager)
                 .build();
     }
+
 }
